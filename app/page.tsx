c1260c6796b391096a11d2a7899ef83380009022
Michael Wu <michaelwu2004@gmail.com>
2024-06-15 09:49:31 -0500

    Add animations and styling changes

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,12 @@ export default function Home() {
   return (
     <>
       <div className="w-full flex flex-col items-center justify-center text-center">
-        <motion.div className="w-48 h-48 relative my-6">
+        <motion.div
+          className="w-48 h-48 relative my-6"
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.5 }}
+        >
           <Image
             className="object-cover rounded"
             id="carouselImage"
@@ -27,7 +32,9 @@ export default function Home() {
         <AnimatedText text={intro} />
         <motion.div
           className="font-mono text-white text-2xl my-5"
-
+          initial={{ y: 50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.3 }}
         >
           {aboutme}
         </motion.div>
@@ -36,23 +43,31 @@ export default function Home() {
           <div className="flex flex-row tabs">
             {sections.map((section, idx) => {
               return (
-                <div
+                <motion.div
                   key={idx}
                   className={`font-mono relative flex-grow px-8 ${section === selectedTab ? 'selected' : ''}`}
                   onClick={() => {
                     console.log(section)
                     setSelectedTab(section)
                   }}
+                  initial={{ y: 50, opacity: 0 }}
+                  animate={{ y: 0, opacity: 1 }}
+                  transition={{ duration: 0.5, delay: 0.4 }}
                 >
                   {section}
                   {section === selectedTab ? (
                     <motion.div className="underline " layoutId="underline" />
                   ) : null}
-                </div>
+                </motion.div>
               )
             })}
           </div>
-          <div className="no-scrollbar h-64 items-center justify-center text-center overflow-auto p-4">
+          <motion.div
+            className="no-scrollbar h-64 items-center justify-center text-center overflow-auto p-4"
+            initial={{ y: 50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.5, delay: 0.6 }}
+          >
             <AnimatePresence mode="wait">
               <motion.div
                 key={selectedTab}
@@ -88,7 +103,7 @@ export default function Home() {
                   : null}
               </motion.div>
             </AnimatePresence>
-          </div>
+          </motion.div>
         </div>
       </div>
     </>
